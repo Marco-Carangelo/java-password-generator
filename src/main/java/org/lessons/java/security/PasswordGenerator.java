0f37c63c7b537d88fbf3845b5a8abd9f875a912d
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-05 16:00:28 +0200

    add: Scanner class imported and Scanner object inizializzed

--- src/main/java/org/lessons/java/security/PasswordGenerator.java
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -1,9 +1,18 @@
 package org.lessons.java.security;
 
+//Importiamo la classe Scanner dalla JRE
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Inizializziamo un'istanza della classe Scanner per poter gestire il flusso di dati in input
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		
+		
 
 	}
 
